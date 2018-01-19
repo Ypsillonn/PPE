7b637778286601e25732c6166ec179581c12bd05
Ypsillonn <35596935+Ypsillonn@users.noreply.github.com>
2018-01-19 11:15:50 +0100

    Add files via upload

---
+++ d_finitif.ino
@@ -0,0 +1,46 @@
+const int ledPin = 13;
+const int buttonPin = 2;
+
+int buttonState = 0;
+float compteur = 0;
+int quantite = 0;
+float stock = 0;
+int temps = 0; 
+byte bascule = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop()
+{
+    buttonState = digitalRead(buttonPin);
+    Serial.print(temps);
+    if(buttonState == HIGH) 
+    {
+      digitalWrite(ledPin, HIGH);
+      compteur = compteur+0.1;
+    }
+    else digitalWrite(ledPin, LOW);
+
+    while(temps == 3600) 
+    {
+      quantite = (int)compteur;
+      stock = compteur-(int)compteur;
+      temps = 0;
+      
+      if(stock < 0.5) stock = 0;
+      else stock = 1;
+      
+      quantite = quantite + stock;
+      Serial.print(quantite);
+    } 
+    delay(1000);
+    temps = temps+1;
+}
+
+
+ 
---
+++ d_finitif_2.ino
@@ -0,0 +1,37 @@
+const int pinBouton = 2;
+
+int etatBouton = 0;
+float compteur = 0;
+int quantite = 0;
+float stock = 0;
+int temps = 0; 
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop()
+{
+    etatBouton = digitalRead(pinBouton);
+    Serial.print(temps);
+    if(etatBouton == HIGH) compteur = compteur+0.1;
+    
+    while(temps == 3600) 
+    {
+      quantite = (int)compteur;
+      stock = compteur-(int)compteur;
+      temps = 0;
+      
+      if(stock < 0.5) stock = 0;
+      else stock = 1;
+      
+      quantite = quantite + stock;
+    } 
+    delay(1000);
+    temps = temps+1;
+}
+
+
+ 
